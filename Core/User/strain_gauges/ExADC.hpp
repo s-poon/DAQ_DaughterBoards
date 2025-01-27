c7c145349954cd46273f010c7f1c7d048be17916
s-poon <stevenpoon12.03@gmail.com>
2025-01-27 13:52:06 -0700

    stuff

--- Core/User/strain_gauges/ExADC.hpp
+++ Core/User/strain_gauges/ExADC.hpp
@@ -19,13 +19,13 @@ class ExADC{
             uint8_t mode
         )= 0;
 
-        virtual int32_t ReadWriteADCData(
-            uint8_t* deviceStatus,
-            uint8_t mode,
-            uint8_t startAdress,
-            uint8_t writeCount,
-            uint8_t* data
-        );
+//        virtual int32_t ReadWriteADCData(
+//            uint8_t* deviceStatus,
+//            uint8_t mode,
+//            uint8_t startAdress,
+//            uint8_t writeCount,
+//            uint8_t* data
+//        );
 
         uint8_t ReadRegister(
             uint8_t address
